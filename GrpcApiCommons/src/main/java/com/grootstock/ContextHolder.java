7893e5c9c3e652aa2b08ccd02d5ab4f5417a7a89
Ankit Dimania <ankitdimania@eligible.com>
2016-06-29 13:13:31 +0530

    Fix request id generation

--- GrpcApiCommons/src/main/java/com/grootstock/ContextHolder.java
+++ GrpcApiCommons/src/main/java/com/grootstock/ContextHolder.java
@@ -1,18 +1,27 @@
 package com.grootstock;
 
-import lombok.Data;
+import lombok.Getter;
 
-import java.util.UUID;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-@Data
 public class ContextHolder {
   private static ThreadLocal<ContextHolder> storage = new ThreadLocal<>();
 
-  private String requestId = UUID.randomUUID().toString();
-  private long threadId = Thread.currentThread().getId();
+  @Getter private String requestId = generateGuid();
+  @Getter private long threadId = Thread.currentThread().getId();
+
+  static SecureRandom numberGenerator = new SecureRandom();
 
   public static ContextHolder get() {
     return storage.get();
+  }
+
+  private static String generateGuid() {
+    byte[] randomBytes = new byte[16];
+    numberGenerator.nextBytes(randomBytes);
+
+    return Base64.getUrlEncoder().encodeToString(randomBytes).replaceAll("=", "");
   }
 
   /**
